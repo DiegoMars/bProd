322624a232148c179a1abb9458cf814334c4380a
MarsEndeavor <131321814+DiegoMars@users.noreply.github.com>
2025-12-16 17:00:13 -0800

    Added vercel + fixed cert

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import fs from 'node:fs';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,17 +13,18 @@ export default defineConfig({
     server: {
       https: {
         key: fs.readFileSync('./certs/localhost-key.pem'),
-        cert: fs.readFileSync('./certs/localhost'),
+        cert: fs.readFileSync('./certs/localhost.pem'),
       },
     },
   },
 
   integrations: [react()],
-
   output: 'server',
 
   // Astro-level server options (no https here)
   server: {
     port: 4321,
   },
+
+  adapter: vercel(),
 });
